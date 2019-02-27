b150afab1573bf3ce542d19474b79fcbb46ee080
Tomasz Strojny <tomasz@init.biz>
2019-02-27 12:48:03 +0100

    Markers model added

---
+++ models/Marker.php
@@ -0,0 +1,37 @@
+<?php namespace Initbiz\LeafletPro\Models;
+
+use Model;
+
+/**
+ * Marker Model
+ */
+class Marker extends Model
+{
+    /**
+     * @var string The database table used by the model.
+     */
+    public $table = 'initbiz_leafletpro_markers';
+
+    /**
+     * @var array Guarded fields
+     */
+    protected $guarded = ['*'];
+
+    /**
+     * @var array Fillable fields
+     */
+    protected $fillable = [];
+
+    /**
+     * @var array Relations
+     */
+    public $hasOne = [];
+    public $hasMany = [];
+    public $belongsTo = [];
+    public $belongsToMany = [];
+    public $morphTo = [];
+    public $morphOne = [];
+    public $morphMany = [];
+    public $attachOne = [];
+    public $attachMany = [];
+}
---
+++ updates/create_markers_table.php
@@ -0,0 +1,28 @@
+<?php namespace Initbiz\LeafletPro\Updates;
+
+use Schema;
+use October\Rain\Database\Schema\Blueprint;
+use October\Rain\Database\Updates\Migration;
+
+class CreateMarkersTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('initbiz_leafletpro_markers', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->string('name');
+            $table->boolean('is_published')->default(true);
+            $table->text('description')->nullable();
+            $table->string('long');
+            $table->string('lat');
+            $table->text('additional_data')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('initbiz_leafletpro_markers');
+    }
+}
