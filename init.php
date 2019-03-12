8bf1788427b9366a3dc4e50a7a74d3109125e0ef
Tomasz Strojny <tomasz@init.biz>
2019-03-12 11:24:00 +0100

    Extending Cumulus fixeD

--- init.php
+++ init.php
@@ -3,113 +3,105 @@
 use Event;
 use System\Classes\PluginManager;
 use Initbiz\LeafletPro\Models\Marker;
-use Initbiz\CumulusCore\Models\Cluster;
 use Initbiz\LeafletPro\Controllers\Markers;
-use Initbiz\CumulusCore\Controllers\Clusters;
 
-Event::listen('backend.form.extendFieldsBefore', function ($formWidget) {
-    if (!$formWidget->model instanceof Marker) {
-        return;
-    }
+if (PluginManager::instance()->exists('Initbiz.CumulusCore')) {
+    Event::listen('backend.form.extendFieldsBefore', function ($formWidget) {
+        if (!$formWidget->model instanceof Marker) {
+            return;
+        }
 
-    if (!PluginManager::instance()->exists('Initbiz.CumulusCore')) {
-        return;
-    }
+        $clusterField = [];
+        $clusterField['cluster_id'] = [
+            'label'   => 'initbiz.leafletpro::lang.marker.cluster',
+            'commentAbove'   => 'initbiz.leafletpro::lang.marker.cluster_comment',
+            'type'   => 'dropdown',
+            'span'   => 'right',
+            'emptyOption'   => 'Select cluster',
+        ];
 
-    $clusterField = [];
-    $clusterField['cluster_id'] = [
-        'label'   => 'initbiz.leafletpro::lang.marker.cluster',
-        'commentAbove'   => 'initbiz.leafletpro::lang.marker.cluster_comment',
-        'type'   => 'dropdown',
-        'span'   => 'right',
-        'emptyOption'   => 'Select cluster',
-    ];
+        $fields = $formWidget->fields;
 
-    $fields = $formWidget->fields;
+        $fields['name']['dependsOn'] = 'cluster_id';
+        $fields['street']['dependsOn'] = 'cluster_id';
+        $fields['postal_code']['dependsOn'] = 'cluster_id';
+        $fields['city']['dependsOn'] = 'cluster_id';
+        $fields['country_id']['dependsOn'] = 'cluster_id';
 
-    $fields['name']['dependsOn'] = 'cluster_id';
-    $fields['street']['dependsOn'] = 'cluster_id';
-    $fields['postal_code']['dependsOn'] = 'cluster_id';
-    $fields['city']['dependsOn'] = 'cluster_id';
-    $fields['country_id']['dependsOn'] = 'cluster_id';
-
-    $formWidget->fields = $clusterField + $fields;
-});
-
-Marker::extend(function ($model) {
-    if (!PluginManager::instance()->exists('Initbiz.CumulusCore')) {
-        return;
-    }
-
-    $model->belongsTo['cluster'] = ['Initbiz\CumulusCore\Models\Cluster'];
-
-    $model->addDynamicMethod('getClusterIdOptions', function () {
-        return Cluster::all()->pluck('name', 'id')->toArray();
+        $formWidget->fields = $clusterField + $fields;
     });
-});
 
-Cluster::extend(function ($model) {
-    $model->hasMany['markers'] = ['Initbiz\LeafletPro\Models\Marker'];
+    Marker::extend(function ($model) {
+        $model->belongsTo['cluster'] = ['Initbiz\CumulusCore\Models\Cluster'];
 
-    $model->bindEvent('model.afterSave', function () use ($model) {
-        if ($model->markers()->count() === 0) {
-            // If cluster has no marker, create one
-            if (!empty($model->city) && !empty($model->thoroughfare) && !empty($model->name)) {
-                $marker = new Marker();
-                $marker->name = $model->name;
-                $marker->cluster_id = $model->id;
-                $marker->street = $model->thoroughfare;
-                $marker->city = $model->city;
-                if (!empty($marker->country_id)) {
-                    $country = Country::find($marker->country_id);
-                    $marker->country()->associate($country);
-                }
-                if (!empty($marker->postal_code)) {
-                    $marker->postal_code = $model->postal_code;
-                }
+        $model->addDynamicMethod('getClusterIdOptions', function () {
+            return \Initbiz\CumulusCore\Models\Cluster::all()->pluck('name', 'id')->toArray();
+        });
+    });
 
-                $marker->refreshLonLat();
-                $marker->save();
-            }
-        } else {
-            // else get first and if address updated than update marker
-            if (!empty($model->city) && !empty($model->thoroughfare) && !empty($model->name)) {
-                $marker = $model->markers()->first();
+    \Initbiz\CumulusCore\Models\Cluster::extend(function ($model) {
+        $model->hasMany['markers'] = ['Initbiz\LeafletPro\Models\Marker'];
 
-                $markerChanged = false;
-
-                if ($marker->city !== $model->city) {
-                    $marker->city = $model->city;
-                    $markerChanged = true;
-                }
-
-                if ($marker->street !== $model->thoroughfare) {
+        $model->bindEvent('model.afterSave', function () use ($model) {
+            if ($model->markers()->count() === 0) {
+                // If cluster has no marker, create one
+                if (!empty($model->city) && !empty($model->thoroughfare) && !empty($model->name)) {
+                    $marker = new Marker();
+                    $marker->name = $model->name;
+                    $marker->cluster_id = $model->id;
                     $marker->street = $model->thoroughfare;
-                    $markerChanged = true;
-                }
+                    $marker->city = $model->city;
+                    if (!empty($marker->country_id)) {
+                        $country = Country::find($marker->country_id);
+                        $marker->country()->associate($country);
+                    }
+                    if (!empty($marker->postal_code)) {
+                        $marker->postal_code = $model->postal_code;
+                    }
 
-                if ($marker->country_id !== $model->country_id) {
-                    $marker->country_id = $model->country_id;
-                    $markerChanged = true;
-                }
-
-                if ($marker->postal_code !== $model->postal_code) {
-                    $marker->postal_code = $model->postal_code;
-                    $markerChanged = true;
-                }
-
-                if ($markerChanged) {
                     $marker->refreshLonLat();
                     $marker->save();
                 }
-            }
-        }
-    });
-});
+            } else {
+                // else get first and if address updated than update marker
+                if (!empty($model->city) && !empty($model->thoroughfare) && !empty($model->name)) {
+                    $marker = $model->markers()->first();
 
-Clusters::extendFormFields(function ($form, $model, $context) {
-    if (!$model instanceof Cluster) {
-        return;
-    }
-    //TODO: add relation controller for managing map markers
-});
+                    $markerChanged = false;
+
+                    if ($marker->city !== $model->city) {
+                        $marker->city = $model->city;
+                        $markerChanged = true;
+                    }
+
+                    if ($marker->street !== $model->thoroughfare) {
+                        $marker->street = $model->thoroughfare;
+                        $markerChanged = true;
+                    }
+
+                    if ($marker->country_id !== $model->country_id) {
+                        $marker->country_id = $model->country_id;
+                        $markerChanged = true;
+                    }
+
+                    if ($marker->postal_code !== $model->postal_code) {
+                        $marker->postal_code = $model->postal_code;
+                        $markerChanged = true;
+                    }
+
+                    if ($markerChanged) {
+                        $marker->refreshLonLat();
+                        $marker->save();
+                    }
+                }
+            }
+        });
+    });
+
+    \Initbiz\CumulusCore\Controllers\Clusters::extendFormFields(function ($form, $model, $context) {
+        if (!$model instanceof \Initbiz\CumulusCore\Models\Cluster) {
+            return;
+        }
+        //TODO: add relation controller for managing map markers
+    });
+}
